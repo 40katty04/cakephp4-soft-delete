b960bc50586e70d4273bd3d08983b9ff42c67a32
40katty04 <40katty04@gmail.com>
2020-09-29 13:10:51 +0300

    update to compability with cakePHP 4.1

--- src/Model/Table/SoftDeleteTrait.php
+++ src/Model/Table/SoftDeleteTrait.php
@@ -182,29 +182,4 @@ trait SoftDeleteTrait {
         $entity->$softDeleteField = null;
         return $this->save($entity);
     }
-
-    /**
-     * Overwriting triggerBeforeFind() to let queries not return soft deleted records
-     *
-     * Cake\ORM\Query::triggerBeforeFind() overwritten to add the condition `deleted IS NULL` to every find request
-     * in order to not return soft deleted records.
-     * If the query contains the option `withDeleted`, the condition `deleted IS NULL` is not applied.
-     *
-     * @return void
-     */
-    public function beforeFind(EventInterface $event, Query $query, ArrayObject $options, $primary)
-    {
-        if (!$this->_beforeFindFired && $this->_type === 'select') {
-            parent::triggerBeforeFind();
-
-            $repository = $this->getRepository();
-            $options = $this->getOptions();
-
-            if (!is_array($options) || !in_array('withDeleted', $options)) {
-                $aliasedField = $repository->aliasField($repository->getSoftDeleteField());
-                $this->andWhere($aliasedField . ' IS NULL');
-            }
-        }
-        return;
-    }
 }
